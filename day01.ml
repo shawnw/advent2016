bc67eac1f6120f5dbbb8de0437e94e5b82bdd611
Shawn <shawnw@speakeasy.org>
2016-12-01 08:34:14 -0800

    Day 1 Part 2

--- day01.ml
+++ day01.ml
@@ -4,6 +4,13 @@ type pair = { x : int; y : int }
 type direction = North | East | West | South
 type rotate = Left | Right
 
+module OrderedPair = struct
+  type t = pair
+  let compare = Pervasives.compare 
+end
+
+module Cache = BatSet.Make(OrderedPair)
+
 let new_direction d r = 
   match d with
   | North when r = Left -> West
@@ -45,6 +52,42 @@ let taxi orig dest =
   and a2 = abs (orig.y - dest.y) in
   a1 + a2
 
+let rec add_points cache orig dest =
+  if orig.x = dest.x && orig.y = dest.y then
+    (false, orig, cache)
+  else begin
+    let newpoint = 
+    if orig.x < dest.x then
+      { orig with x = orig.x + 1 }
+    else if orig.x > dest.x then
+      { orig with x = orig.x - 1 }
+    else if orig.y < dest.y then
+      { orig with y = orig.y + 1 }
+    else if orig.y > dest.y then
+      { orig with y = orig.y - 1 } 
+    else
+      orig in
+    if Cache.mem newpoint cache then
+      (true, newpoint, cache)
+    else begin
+      (* Printf.printf "Adding {%d,%d} to cache.\n" newpoint.x newpoint.y; *)
+      add_points (Cache.add newpoint cache) newpoint dest
+    end
+  end
+
+let first_repeat dirs loc =
+  let points = Cache.add (snd loc) Cache.empty in
+  let rec helper loc cache = function
+    | hd :: tl ->
+      let (newdir, newloc) = move hd loc in
+      let (found, repeat, cache) = add_points cache (snd loc) newloc in
+      if found then
+	repeat
+      else
+	helper (newdir, newloc) cache tl
+    | [] -> raise (Invalid_argument "No repeated coordinates!") in
+  helper loc points dirs
+
 
 let main () = 
   let startc = {x = 0; y = 0 }
@@ -52,7 +95,10 @@ let main () =
   and dirs = BatString.nsplit (input_all Pervasives.stdin) "," in
   List.iter (fun d -> currc := move d !currc) dirs;
   Printf.printf "Ending coordinates: x = %d, y = %d\n" (snd !currc).x (snd !currc).y;
-  Printf.printf "Distance: %d\n" (taxi startc (snd !currc))
+  Printf.printf "Distance: %d\n" (taxi startc (snd !currc));
+  let repeat = first_repeat dirs (North, { x = 0; y = 0}) in
+  Printf.printf "First repeated coordinate: x = %d, y = %d\n" repeat.x repeat.y;
+  Printf.printf "Distance: %d\n" (taxi startc repeat)
 
 
 let _ = main ()
