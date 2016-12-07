c4671d618627726da361ae9828706fb483ebda23
Shawn <shawnw@speakeasy.org>
2016-12-06 17:29:42 -0800

    Clean up day01 a bit

--- day01.ml
+++ day01.ml
@@ -4,29 +4,22 @@ type pair = { x : int; y : int }
 type direction = North | East | West | South
 type rotate = Left | Right
 
-module OrderedPair = struct
-  type t = pair
-  let compare = Pervasives.compare 
-end
-
-module Cache = BatSet.Make(OrderedPair)
 
 let new_direction d r = 
   match d with
   | North when r = Left -> West
-  | North when r = Right -> East
+  | North -> East
   | East when r = Left -> North
-  | East when r = Right -> South
+  | East -> South
   | South when r = Left -> East
-  | South when r = Right -> West
+  | South -> West
   | West when r = Left -> South
-  | West when r = Right -> North
+  | West -> North
 
 let to_direction = function
   | "L" -> Left
   | "R" -> Right
   | _ -> raise (Invalid_argument "Direction must be L or R")
-
 
 let adjust dir dist pos =
   match dir with
@@ -34,7 +27,6 @@ let adjust dir dist pos =
   | East -> { pos with x = pos.x + dist }
   | South -> { pos with y = pos.y - dist }
   | West -> { pos with x = pos.x - dist }
-
 
 let re = Str.regexp " *\\([LR]\\)\\([0-9]+\\) *"
 
@@ -53,7 +45,7 @@ let taxi orig dest =
   a1 + a2
 
 let rec add_points cache orig dest =
-  if orig.x = dest.x && orig.y = dest.y then
+  if orig = dest then
     (false, orig, cache)
   else begin
     let newpoint = 
@@ -67,16 +59,14 @@ let rec add_points cache orig dest =
       { orig with y = orig.y - 1 } 
     else
       orig in
-    if Cache.mem newpoint cache then
+    if BatSet.mem newpoint cache then
       (true, newpoint, cache)
-    else begin
-      (* Printf.printf "Adding {%d,%d} to cache.\n" newpoint.x newpoint.y; *)
-      add_points (Cache.add newpoint cache) newpoint dest
-    end
+    else
+      add_points (BatSet.add newpoint cache) newpoint dest
   end
 
 let first_repeat dirs loc =
-  let points = Cache.add (snd loc) Cache.empty in
+  let points = BatSet.add (snd loc) BatSet.empty in
   let rec helper loc cache = function
     | hd :: tl ->
       let (newdir, newloc) = move hd loc in
@@ -88,7 +78,6 @@ let first_repeat dirs loc =
     | [] -> raise (Invalid_argument "No repeated coordinates!") in
   helper loc points dirs
 
-
 let main () = 
   let startc = {x = 0; y = 0 }
   and currc = ref (North, { x = 0; y = 0 })
@@ -99,7 +88,6 @@ let main () =
   let repeat = first_repeat dirs (North, { x = 0; y = 0}) in
   Printf.printf "First repeated coordinate: x = %d, y = %d\n" repeat.x repeat.y;
   Printf.printf "Distance: %d\n" (taxi startc repeat)
-
 
 let _ = main ()
       
