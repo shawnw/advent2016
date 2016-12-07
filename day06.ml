70f0882fe058fd1945f57a1ff3981f393c60438f
Shawn <shawnw.mobile@gmail.com>
2016-12-06 22:37:41 -0800

    Day 06 Part 2

--- day06.ml
+++ day06.ml
@@ -9,11 +9,18 @@ let _ =
              ch
              (function Some i -> Some (i + 1) | None -> Some 1)
              cols.(i)));
+  print_string "Part 1 (Most common): ";
   BatArray.iter (fun tbl ->
       let ch, v = 
         BatHashtbl.fold (fun ch v mx ->
             if v > snd mx then (ch, v) else mx) tbl ('_', -1) in
         print_char ch) cols;
+  print_newline ();
+  print_string "Part 2 (Least common): ";
+  BatArray.iter (fun tbl ->
+      let ch, v = 
+        BatHashtbl.fold (fun ch v mn ->
+            if v < snd mn then (ch, v) else mn) tbl ('_', max_int) in
+        print_char ch) cols;
   print_newline ()
-                
                                  
