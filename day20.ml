7ff87c3a0d33298925dd046d851390d0f6e4fa0b
Shawn <shawnw.mobile@gmail.com>
2016-12-29 16:04:04 -0800

    Another tweak to Day 20

--- day20.ml
+++ day20.ml
@@ -7,13 +7,13 @@ let _ =
     BatIO.lines_of stdin 
     |> BatEnum.fold (fun iset line ->
            if BatString.contains line '-' then
-             BatScanf.sscanf line "%d-%d" (fun lo hi -> BatISet.add_range lo hi iset)
+             BatScanf.sscanf line "%d-%d"
+                             (fun lo hi -> BatISet.add_range lo hi iset)
            else
              BatISet.add (int_of_string line) iset) BatISet.empty in
   let max_num = 4294967295 in
-  let whitelist = BatISet.compl blacklist
-                  |> BatISet.remove_range BatInt.min_num (-1)
-                  |> BatISet.remove_range (max_num + 1) BatInt.max_num in
+  let whitelist = BatISet.diff (BatISet.add_range 0 max_num BatISet.empty)
+                               blacklist in
   let lowest = BatISet.min_elt whitelist
   and count = BatISet.cardinal whitelist in
   BatPrintf.printf "Part 1: Lowest valid number: %d\n" lowest;
